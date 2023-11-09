a7895153625a61d9b9c764a78c45d572f96e7538
psychopomp3 <psychopompe@protonmail.com>
2023-11-09 16:30:56 +0100

    virer ramsey/uuid

--- src/index.php
+++ src/index.php
@@ -1,3 +1,14 @@
 <?php
-    echo 'bom dia';
-?>
+
+require_once './vendor/autoload.php';
+
+use Ramsey\Uuid\Uuid;
+
+$uuid = Uuid::uuid4();
+
+
+printf(
+    "UUID: %s\nVersion: %d\n",
+    $uuid->toString(),
+    $uuid->getFields()->getVersion()
+);
